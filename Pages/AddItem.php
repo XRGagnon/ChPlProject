55c8a70e02093c19d4130606c28a61c5eb7bb1b7
Michael <alphastudio99@gmail.com>
2019-01-15 16:38:42 -0500

    Modified the File to accommodate the new database

--- Pages/AddItem.php
+++ Pages/AddItem.php
@@ -8,18 +8,10 @@ DefaultHead();
 	<table>
 		<tr>
 			<td style="text-align: right; margin-left: 10px;">
-				<label for="itemname">Item Name: </label>
+				<label for="Item_No">Item Number: </label>
 			</td>
 			<td>
-				<input type="text" name="itemname" id="itemname"><br>
-			</td>
-		</tr>
-		<tr>
-			<td style="text-align: right; margin-left: 10px;">
-				<label for="price">Item Price: </label>
-			</td>
-			<td>
-				<input type="text" name="price" id="price"><br><br>
+				<input type="text" name="Item_No" id="Item_No"><br>
 			</td>
 		</tr>
 		<tr>
@@ -28,14 +20,31 @@ DefaultHead();
 			</td>
 			<td>
 				<select name="category">
-				  <option value="ACCESSORIES">ACCESSORIES</option>
-				  <option value="MAINTENANCE">MAINTENANCE</option>
-				  <option value="CHEMICALS">CHEMICALS</option>
-				  <option value="EQUIPMENT">EQUIPMENT</option>
-				  <option value="OTHER">OTHER</option>
+				  <option value="1">New Products</option>
+				  <option value="2">Maintenance</option>
+				  <option value="3">Vacuum Hoses</option>
+				  <option value="4">Accessories</option>
+				  <option value="5">Backwash Hoses</option>
+				  <option value="6">Skimmers & Drains</option>
+				  <option value="7">Plumbing</option>
+				  <option value="8">Ladders & Steps</option>
+				  <option value="9">Lights</option>
+				  <option value="10">Cover Reels & Solar Rollers</option>
+				  <option value="11">Games, Chairs & More</option>
 				</select>
 			</td>
 		</tr>
+		<tr>
+			<td style="text-align: right; margin-left: 10px;">
+				<label for="Availability">Availability</label>
+			</td>
+			<td>
+				<input type="radio" name="Availability" id="Both" checked><br>
+				<input type="radio" name="Availability" id="Platinum"><br>
+				<input type="radio" name="Availability" id="Olympic"><br>
+			</td>
+		</tr>
+		<tr>
 		<tr>
 			<td>
 			</td>
